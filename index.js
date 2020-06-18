d09d5ae452557e6adf97481d0160923e15f55a13
smessina <silvio.messina@eant.tech>
2020-06-18 20:36:27 -0300

    Integrando Multer y Joi para datos HTTP

--- index.js
+++ index.js
@@ -1,21 +1,54 @@
 const express = require('express')
+const multer = require('multer')
+const joi = require("@hapi/joi")
 const server = express()
 
 const public = express.static("public")
-const urlencoded = express.urlencoded({ extended : false })
+const urlencoded = express.urlencoded({ extended : true })
 const json = express.json()
+const upload = multer()
 
-server.use( public ) //<-- Middleware
-server.use( urlencoded )
+const schemaContact = joi.object({
+    nombre : joi.string().alphanum().min(4).max(25).required(),
+    correo : joi.string().email({
+        minDomainSegments: 2,
+        tlds: {
+            allow: ['com', 'net', 'org']
+        }
+    }).required(),
+    asunto : joi.string().alphanum().valid("ax14", "ax38", "ax45", "ax67").required(),
+    mensaje : joi.string().alphanum().min(10).max(100).required()
+})
+
 server.use( json )
+server.use( urlencoded )
+server.use( upload.array() )
+server.use( public ) //<-- Middleware
  
 server.post('/enviar', function (request, response) {
-    const datos = request.body
+    //const datos = request.body //<-- Proximamente...
+    const datos = {
+        nombre : "Luis Miguel",
+        correo : "luis@miguel.com",
+        asunto : "ax14",
+        mensaje : "No culpes a la noche..."
+    }
 
     console.log("Estos son los datos enviados:")
     console.log( datos )
+
+    const validacion = schemaContact.validate( datos )
+
+    if( validacion.error ){
+
+        response.json({ rta : "error" })
+
+    } else {
+
+        response.json({ rta : "ok" })
+
+    }
     
-    response.json({ rta : "ok" })
 })
  
 server.listen(3000)
