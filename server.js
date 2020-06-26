81cd94e2970352faf5667b8a48ac0398ad36c373
smessina <silvio.messina@eant.tech>
2020-06-25 22:14:58 -0300

    Integrando con MongoDB Atlas

--- server.js
+++ server.js
@@ -1,19 +1,28 @@
 const express = require('express')
+const { MongoClient } = require('mongodb')
 const server = express()
 
 const urlencoded = express.urlencoded({ extended : true })
 const json = express.json()
 
-const DB = []
+const url = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@${process.env.MONGODB_HOST}/${process.env.MONGODB_BASE}?retryWrites=true&w=majority`
 
-console.log( DB )
+let DB = null
+
+MongoClient.connect(url, { useUnifiedTopology: true }, function(error, client) {
+    
+    DB = client.db("MercadoTECH")
+    
+})
+
 
 server.use( json )
 server.use( urlencoded )
 server.listen(3000)
 
 server.get("/api", (req, res) => { //<-- Obtener los datos
-    res.json( DB )
+    console.log( DB.collection('Productos').find({}).toArray() )
+    res.json( DB.collection('Productos').find({}).toArray() )
 })
 
 server.post("/api", (req, res) => { //<-- Crear con datos
